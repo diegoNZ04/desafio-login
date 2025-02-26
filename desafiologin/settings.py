17412717d027c59cd71688344c6e803745dde9ef
diegoNZ04 <diegoamorim03152004@gmail.com>
2025-02-26 14:42:41 -0300

    Adicionando envio de Emails

--- desafiologin/settings.py
+++ desafiologin/settings.py
@@ -1,3 +1,4 @@
+import environ
 import os
 from pathlib import Path
 
@@ -90,6 +91,19 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Django-Eviron
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+# Email Configuration
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env("EMAIL_HOST_USER")
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
